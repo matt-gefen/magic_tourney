eebd6c4a3a3dac23e18b8dd91db2fbb1bffa39cb
mattgefen <matt.gefen@gmail.com>
2021-10-16 13:56:47 -0600

    startClick function

--- js/app.js
+++ js/app.js
@@ -14,20 +14,24 @@ let round = null
 const mainContainer = document.querySelector('.container')
 const mainMessage = document.querySelector('#main-message')
 const startButton = document.querySelector('#start')
+const selectButton = document.querySelector('#select')
 const playerSelect = document.querySelector('.player-selection')
 
 
 // event listeners
-startButton.addEventListener("click", function(evt) {
-  mainMessage.innerText = 'Select Your Contestant'
-  startButton.innerText = 'Step Into Arena'
-  playerSelect.removeAttribute('hidden')
-
-})
+startButton.addEventListener("click", startClick)
 
 console.log(characters.playerCharacters)
 
 // functions
 function init() {
   return
+}
+
+function startClick(evt) {
+  mainMessage.innerText = 'Select Your Contestant'
+  startButton.innerText = 'Step Into Arena'
+  playerSelect.removeAttribute('hidden')
+  selectButton.removeAttribute('hidden')
+  startButton.hidden = true
 }
